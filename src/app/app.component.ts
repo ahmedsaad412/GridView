108df43fb27415f42880807f57e9897279178dac
Ahmed Hilmy <ahmed.hilmy@alqemam.com>
2024-07-14 16:29:30 +0300

    dynamic Grid View

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
+import { DynamicGridComponent } from './components/dynamic-grid/dynamic-grid.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,DynamicGridComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'GridView';
+
+  Headers: string[] = ['id', 'status', 'complain_provider' ,'mangement','for','title','IsSelected'];
+  Tickets: any[] = [
+    { id: 1,status:"normal", complain_provider:"yasmine" ,mangement:"technical support" ,for:"ahmed" ,title: "Title ",  IsSelected: false },
+    { id: 2,status:"normal", complain_provider:"yasmine" ,mangement:"technical support" ,for:"ahmed" ,title: "Title ",  IsSelected: false },
+    { id: 3,status:"normal", complain_provider:"ali" ,    mangement:"technical support" ,for:"Ahmed" ,title: "Title ",  IsSelected: false },
+    { id: 4,status:"normal", complain_provider:"ali" ,    mangement:"technical support" ,for:"Ahmed" ,title: "Title ",  IsSelected: false },
+    { id: 5,status:"normal", complain_provider:"ali" ,    mangement:"technical support" ,for:"Ahmed" ,title: "Title ",  IsSelected: false },
+    { id: 6,status:"normal", complain_provider:"ali" ,    mangement:"technical support" ,for:"Ahmed" ,title: "Title ",  IsSelected: false },
+    { id: 7,status:"normal", complain_provider:"yasmine" ,mangement:"technical support" ,for:"Sara" , title: "Title ",  IsSelected: false },
+    { id: 8,status:"normal", complain_provider:"yasmine" ,mangement:"technical support" ,for:"Sara" , title: "Title ",  IsSelected: false },
+    { id: 9,status:"normal", complain_provider:"yasmine" ,mangement:"technical support" ,for:"Sara" , title: "Title ",  IsSelected: false },
+    { id: 10,status:"normal", complain_provider:"yasmine" ,mangement:"technical support" ,for:"Sara" , title: "Title ", IsSelected: false },
+    { id: 11,status:"normal", complain_provider:"omar" ,   mangement:"technical support" ,for:"Mona" , title: "Title ", IsSelected: false },
+    { id: 12,status:"normal", complain_provider:"omar" ,   mangement:"technical support" ,for:"Mona" , title: "Title ", IsSelected: false },
+    { id: 13,status:"normal", complain_provider:"omar" ,   mangement:"technical support" ,for:"Mona" , title: "Title ", IsSelected: false },
+    { id: 14,status:"normal", complain_provider:"omar" ,   mangement:"technical support" ,for:"Mona" , title: "Title ", IsSelected: false }
+  ];
+}
+{
+
 }
---
+++ src/app/components/dynamic-grid/dynamic-grid.component.ts
@@ -0,0 +1,14 @@
+import { Component ,Input} from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-dynamic-grid',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './dynamic-grid.component.html',
+  styleUrl: './dynamic-grid.component.css'
+})
+export class DynamicGridComponent {
+  @Input() headers: string[] = [];
+  @Input() data: any[] = [];
+}
