d8bcab84bd6996fd4400b6c7ac648b12b2685eae
Ahmed Hilmy <ahmedshilmy8811@gmail.com>
2024-07-15 13:57:04 +0300

    router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { DynamicGridComponent } from './components/dynamic-grid/dynamic-grid.component';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,DynamicGridComponent],
+  imports: [RouterOutlet,DynamicGridComponent,RouterModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -14,22 +14,25 @@ export class AppComponent {
   Headers: string[] = ['id', 'status', 'complain_provider' ,'mangement','for','title'];
   Tickets: any[] = [
     { id: 1,status:"normal", complain_provider:"yasmine" ,mangement:"technical support" ,for:"ahmed" ,title: "Title ",  IsSelected: false },
-    { id: 2,status:"normal", complain_provider:"yasmine" ,mangement:"technical support" ,for:"ahmed" ,title: "Title ",  IsSelected: false },
+    { id: 2,status:"normal", complain_provider:"yasmine" ,mangement:"technical support" ,for:"shawqi" ,title: "Title ",  IsSelected: false },
     { id: 3,status:"normal", complain_provider:"ali" ,    mangement:"technical support" ,for:"Ahmed" ,title: "Title ",  IsSelected: false },
-    { id: 4,status:"normal", complain_provider:"ali" ,    mangement:"technical support" ,for:"Ahmed" ,title: "Title ",  IsSelected: false },
-    { id: 5,status:"normal", complain_provider:"ali" ,    mangement:"technical support" ,for:"Ahmed" ,title: "Title ",  IsSelected: false },
-    { id: 6,status:"normal", complain_provider:"ali" ,    mangement:"technical support" ,for:"Ahmed" ,title: "Title ",  IsSelected: false },
+    { id: 4,status:"normal", complain_provider:"ali" ,    mangement:"IT" ,for:"fayez" ,title: "Title ",  IsSelected: false },
+    { id: 5,status:"normal", complain_provider:"ali" ,    mangement:"IT" ,for:"Ahmed" ,title: "Title ",  IsSelected: false },
+    { id: 6,status:"normal", complain_provider:"ali" ,    mangement:"IT" ,for:"azmy" ,title: "Title ",  IsSelected: false },
     { id: 7,status:"normal", complain_provider:"yasmine" ,mangement:"technical support" ,for:"Sara" , title: "Title ",  IsSelected: false },
-    { id: 8,status:"normal", complain_provider:"yasmine" ,mangement:"technical support" ,for:"Sara" , title: "Title ",  IsSelected: false },
+    { id: 8,status:"normal", complain_provider:"yasmine" ,mangement:"technical support" ,for:"sss" , title: "Title ",  IsSelected: false },
     { id: 9,status:"normal", complain_provider:"yasmine" ,mangement:"technical support" ,for:"Sara" , title: "Title ",  IsSelected: false },
-    { id: 10,status:"normal", complain_provider:"yasmine" ,mangement:"technical support" ,for:"Sara" , title: "Title ", IsSelected: false },
+    { id: 10,status:"normal", complain_provider:"yasmine" ,mangement:"technical support" ,for:"nada" , title: "Title ", IsSelected: false },
     { id: 11,status:"normal", complain_provider:"omar" ,   mangement:"technical support" ,for:"Mona" , title: "Title ", IsSelected: false },
-    { id: 12,status:"normal", complain_provider:"omar" ,   mangement:"technical support" ,for:"Mona" , title: "Title ", IsSelected: false },
-    { id: 13,status:"normal", complain_provider:"omar" ,   mangement:"technical support" ,for:"Mona" , title: "Title ", IsSelected: false },
-    { id: 14,status:"normal", complain_provider:"omar" ,   mangement:"technical support" ,for:"Mona" , title: "Title ", IsSelected: false }
+    { id: 12,status:"normal", complain_provider:"nader" ,   mangement:".NET" ,for:"Mona" , title: "Title ", IsSelected: false },
+    { id: 13,status:"normal", complain_provider:"omar" ,   mangement:".NET" ,for:"osman" , title: "Title ", IsSelected: false },
+    { id: 14,status:"normal", complain_provider:"omar" ,   mangement:".NET" ,for:"Mona" , title: "Title ", IsSelected: false },
+    { id: 15,status:"normal", complain_provider:"omar" ,   mangement:"echnical support" ,for:"Mona" , title: "Title ", IsSelected: false },
   ];
+
+
 }
+
+
 {
-
-
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { TicketComponent } from './components/dynamic-grid/ticket/ticket.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"ticket/:id" ,component :TicketComponent}
+];
